4d282db213b6bd97d025b0c96c55196e6dabddeb
Allan <shuttxx09@gmail.com>
2024-03-15 14:50:43 -0300

    put delete cpfinvalido

--- controllers/personController.js
+++ controllers/personController.js
@@ -1,4 +1,5 @@
 const pessoas = require('../models/personModel')
+const { validarCPF } = require('../uteis/util')
 
 const listPerson = async(req, res) => {
     try{
@@ -13,6 +14,10 @@ const createPerson = async(req, res) => {
     try {
         const data = {...req.body}
         data.DataNascimento = new Date(data.DataNascimento)
+        cpf = data.CPF
+        if(!validarCPF(cpf)){
+            return res.status(400).json({ error: 'CPF inválido' })
+        }
         const pessoa = await pessoas.create({...data})
         res.send({...pessoa.dataValues})
     } catch (error) {
@@ -20,4 +25,50 @@ const createPerson = async(req, res) => {
     }
 }
 
-module.exports = { listPerson, createPerson};
+const updatePerson = async(req, res) => {
+    try {
+        const { id } = req.params
+        const data = {...req.body}
+        data.DataNascimento = new Date(data.DataNascimento)
+        const pessoa = await pessoas.findByPk(id)
+        if(!pessoa){
+            return res.status(404).json({ error: 'Pessoa não encontrada' })
+        }
+        await pessoa.update(data)
+        res.status(204).send()
+    } catch (error) {
+        return res.status(500).send(error)
+    }
+}
+
+const deletePerson = async(req, res) => {
+    try {
+        const { id } = req.params
+        const pessoa = await pessoas.findByPk(id)
+        if(!pessoa){
+            return res.status(404).json({ error: 'Pessoa não encontrada' })
+        }
+        await pessoa.destroy()
+        res.status(204).send()
+    } catch (error) {
+        return res.status(500).send(error)
+    }
+}
+
+const getCPF = async (req, res) => {
+    try {
+        const { cpf } = req.params
+        if(!validarCPF(cpf)){
+            return res.status(400).json({ error: 'CPF inválido' })
+        }
+        const pessoa = await pessoas.findOne({ where: {CPF: cpf} })
+        if(!pessoa){
+            return res.status(404).json({ error: 'CPF não encontrada' });
+        }
+        return res.json(pessoa.dataValues)
+    } catch (error) {
+        return res.status(500).send(error)
+    }
+}
+
+module.exports = { listPerson, createPerson, deletePerson, updatePerson, getCPF};
--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -1,8 +1,11 @@
 const { Router } = require('express')
 const pessoaRouter = Router()
-const { createPerson, listPerson } = require('../controllers/personController')
+const { createPerson, listPerson, deletePerson, updatePerson, getCPF } = require('../controllers/personController')
 
 pessoaRouter.get('/', listPerson)
 pessoaRouter.post('/', createPerson)
+pessoaRouter.put('/:id', updatePerson)
+pessoaRouter.delete('/:id', deletePerson)
+pessoaRouter.get('/cpf/:cpf', getCPF);
 
 module.exports = pessoaRouter;
