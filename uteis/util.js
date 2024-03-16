6f46af228b06923234c17d794a2f11de2e1a4faf
Allan Sergio <106421236+shutxx@users.noreply.github.com>
2024-03-15 23:58:43 -0300

    alteração no validados cpf

--- uteis/util.js
+++ uteis/util.js
@@ -3,21 +3,21 @@ function validarCPF(strCPF) {
     var Soma;
     var Resto;
     Soma = 0;
-    if (cpf == "00000000000") return false;
+    if (cpf.length < 10 || cpf.length > 11) return false
 
-    for (i = 1; i <= 9; i++) Soma = Soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
-    Resto = (Soma * 10) % 11;
+    for (i = 1; i <= 9; i++) Soma = Soma + parseInt(cpf.substring(i - 1, i)) * (11 - i)
+    Resto = (Soma * 10) % 11
 
-    if ((Resto == 10) || (Resto == 11)) Resto = 0;
-    if (Resto != parseInt(cpf.substring(9, 10))) return false;
+    if ((Resto == 10) || (Resto == 11)) Resto = 0
+    if (Resto != parseInt(cpf.substring(9, 10))) return false
 
     Soma = 0;
-    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(cpf.substring(i - 1, i)) * (12 - i);
-    Resto = (Soma * 10) % 11;
+    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(cpf.substring(i - 1, i)) * (12 - i)
+    Resto = (Soma * 10) % 11
 
-    if ((Resto == 10) || (Resto == 11)) Resto = 0;
-    if (Resto != parseInt(cpf.substring(10, 11))) return false;
-    return true;
+    if ((Resto == 10) || (Resto == 11)) Resto = 0
+    if (Resto != parseInt(cpf.substring(10, 11))) return false
+    return true
 }
 
 module.exports = { validarCPF };
