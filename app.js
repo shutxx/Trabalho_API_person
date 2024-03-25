424f1dcef55bb2998dbb54ecf331867060b38736
allan sergio <shuttxx09@gmail.com>
2024-03-25 19:37:55 -0300

    cors

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express')
+const cors = require('cors')
 const dataBase = require('./src/infrastructure/db')
 const bodyParser = require('body-parser')
 const pessoaRouter = require('./src/routes/personRoutes')
 
 const app = express()
-const port = 3000
+const port = 8000
 
 dataBase.sync()
 
+app.use(cors())
 app.use(express.json())
 app.use('/person', pessoaRouter)
 app.use(bodyParser.urlencoded({ extended: true }))
